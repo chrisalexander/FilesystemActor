9212c414accbd82d406ce0ef8168fd15fa959eb4
Chris Alexander <chris@chris-alexander.co.uk>
2017-07-31 20:45:32 +0100

    Filesystem tests

---
+++ Filesystem.Tests/CreateFolder.Tests.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using Akka.Actor;
+using Akka.TestKit.VsTest;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Filesystem.Tests
+{
+    [TestClass]
+    public class CreateFolderTests : TestKit
+    {
+        private WriteableFolder folder;
+        private string folderName;
+        private string expectedFullPath;
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            Directory.Delete(this.expectedFullPath);
+            Shutdown();
+        }
+
+        [TestInitialize]
+        public void Initialise()
+        {
+            this.folder = new WriteableFolder(Path.GetTempPath());
+            this.folderName = "TestFolder";
+            this.expectedFullPath = Path.Combine(this.folder.Path, this.folderName);
+        }
+
+        [TestMethod]
+        public void Create_folder()
+        {
+            var fs = Sys.ActorOf(Props.Create(() => new Filesystem()));
+            fs.Tell(new CreateFolder(this.folder, this.folderName));
+            var result = ExpectMsg<WriteableFolder>();
+            Assert.AreEqual(result.Path, this.expectedFullPath);
+            Assert.IsTrue(Directory.Exists(this.expectedFullPath));
+        }
+    }
+}
