6aba199cc9fa2de2461b86bd5aff66c2b4f616ea
Chris Alexander <chris@chris-alexander.co.uk>
2017-08-01 20:16:33 +0100

    More filesystem messages

--- Filesystem/Messages.cs
+++ Filesystem/Messages.cs
@@ -2,9 +2,16 @@
 {
     public class FolderExists
     {
-        public FolderExists(Folder Folder) => this.Folder = Folder;
+        public FolderExists(ReadOnlyFolder Folder) => this.Folder = Folder;
 
-        public Folder Folder { get; }
+        public ReadOnlyFolder Folder { get; }
+    }
+
+    public class FileExists
+    {
+        public FileExists(ReadOnlyFile File) => this.File = File;
+
+        public ReadOnlyFile File { get; }
     }
 
     public class CreateFolder
@@ -18,5 +25,32 @@
         public WriteableFolder Folder { get; }
 
         public string FolderName { get; }
+    }
+
+    public class CreateFile
+    {
+        public CreateFile(WriteableFile File) => this.File = File;
+
+        public WriteableFile File { get; }
+    }
+
+    public class CreateFileInFolder
+    {
+        public CreateFileInFolder(WriteableFolder Folder, string FileName)
+        {
+            this.Folder = Folder;
+            this.FileName = FileName;
+        }
+
+        public WriteableFolder Folder { get; }
+
+        public string FileName { get; }
+    }
+
+    public class DeleteFolder
+    {
+        public DeleteFolder(WriteableFolder Folder) => this.Folder = Folder;
+
+        public WriteableFolder Folder { get; }
     }
 }
