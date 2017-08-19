ca8bf53936f4777a8cb6a2d03fb4c90f5a6e80ce
Chris Alexander <chris@chris-alexander.co.uk>
2017-08-19 15:51:58 +0100

    Let's rebel with more than one class in each file

--- Filesystem.Akka.Tests/DeleteFile.Tests.cs
+++ Filesystem.Akka.Tests/DeleteFile.Tests.cs
@@ -7,6 +7,36 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Filesystem.Akka.Tests
 {
     [TestClass]
+    public class DeleteFileExistsTests : TestKit
+    {
+        private string existingFile;
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            Shutdown();
+            File.Delete(this.existingFile);
+        }
+
+        [TestInitialize]
+        public void Initialise()
+        {
+            this.existingFile = Path.Combine(Path.GetTempPath(), "existing_" + Guid.NewGuid().ToString());
+            File.WriteAllText(this.existingFile, "Test");
+        }
+
+        [TestMethod]
+        public void Can_delete_existing_file()
+        {
+            var fs = Sys.ActorOf(Props.Create(() => new Filesystem()));
+            fs.Tell(new DeleteFile(new DeletableFile(this.existingFile)));
+            var result = ExpectMsg<bool>();
+            Assert.IsTrue(result);
+            Assert.IsFalse(File.Exists(this.existingFile));
+        }
+    }
+
+    [TestClass]
     public class DeleteFileMissingTests : TestKit
     {
         private string missingFile;
--- Filesystem.Akka.Tests/DeleteFolder.Tests.cs
+++ Filesystem.Akka.Tests/DeleteFolder.Tests.cs
@@ -7,6 +7,50 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Filesystem.Akka.Tests
 {
     [TestClass]
+    public class DeleteFolderEmptyTests : TestKit
+    {
+        private string emptyFolder;
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            Shutdown();
+            try
+            {
+                Directory.Delete(this.emptyFolder, true);
+            }
+            catch { }
+        }
+
+        [TestInitialize]
+        public void Initialise()
+        {
+            this.emptyFolder = Path.Combine(Path.GetTempPath(), "empty_" + Guid.NewGuid().ToString());
+            Directory.CreateDirectory(this.emptyFolder);
+        }
+
+        [TestMethod]
+        public void Can_delete_empty_folder_non_recursive()
+        {
+            var fs = Sys.ActorOf(Props.Create(() => new Filesystem()));
+            fs.Tell(new DeleteFolder(new DeletableFolder(this.emptyFolder)));
+            var result = ExpectMsg<bool>();
+            Assert.IsTrue(result);
+            Assert.IsFalse(Directory.Exists(this.emptyFolder));
+        }
+
+        [TestMethod]
+        public void Can_delete_empty_folder_recursive()
+        {
+            var fs = Sys.ActorOf(Props.Create(() => new Filesystem()));
+            fs.Tell(new DeleteFolder(new DeletableFolder(this.emptyFolder)) { Recursive = true });
+            var result = ExpectMsg<bool>();
+            Assert.IsTrue(result);
+            Assert.IsFalse(Directory.Exists(this.emptyFolder));
+        }
+    }
+
+    [TestClass]
     public class DeleteFolderPopulatedTests : TestKit
     {
         private string populatedFolder;
