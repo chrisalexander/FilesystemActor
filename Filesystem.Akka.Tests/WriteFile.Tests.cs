fe7dab50855cfcc5b38ab9329f843ffec79d4a17
Chris Alexander <chris@chris-alexander.co.uk>
2017-08-19 16:01:29 +0100

    Write file

--- Filesystem.Akka.Tests/WriteFile.Tests.cs
+++ Filesystem.Akka.Tests/WriteFile.Tests.cs
@@ -8,19 +8,15 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Filesystem.Akka.Tests
 {
     [TestClass]
-    public class WriteFileTests : TestKit
+    public class WriteFileExistsTests : TestKit
     {
         private string existingFile;
-        private string createFile;
-        private string createFileStream;
 
         [TestCleanup]
         public void Cleanup()
         {
             Shutdown();
             File.Delete(this.existingFile);
-            File.Delete(this.createFile);
-            File.Delete(this.createFileStream);
         }
 
         [TestInitialize]
@@ -28,8 +24,6 @@ namespace Filesystem.Akka.Tests
         {
             this.existingFile = Path.Combine(Path.GetTempPath(), "exists_" + Guid.NewGuid().ToString());
             File.WriteAllText(this.existingFile, "Test");
-            this.createFile = Path.Combine(Path.GetTempPath(), "create_" + Guid.NewGuid().ToString());
-            this.createFileStream = Path.Combine(Path.GetTempPath(), "createstream_" + Guid.NewGuid().ToString());
         }
 
         [TestMethod]
@@ -40,7 +34,26 @@ namespace Filesystem.Akka.Tests
             var result = ExpectMsg<Failure>();
             Assert.IsTrue(result.Exception is IOException);
         }
+    }
 
+    [TestClass]
+    public class WriteFileTests : TestKit
+    {
+        private string createFile;
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            Shutdown();
+            File.Delete(this.createFile);
+        }
+
+        [TestInitialize]
+        public void Initialise()
+        {
+            this.createFile = Path.Combine(Path.GetTempPath(), "create_" + Guid.NewGuid().ToString());
+        }
+        
         [TestMethod]
         public void Can_write_absent_file()
         {
@@ -51,7 +64,26 @@ namespace Filesystem.Akka.Tests
             Assert.IsTrue(File.Exists(this.createFile));
             Assert.AreEqual("Test", File.ReadAllText(this.createFile));
         }
+    }
 
+    [TestClass]
+    public class WriteFileStreamTests : TestKit
+    {
+        private string createFileStream;
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            Shutdown();
+            File.Delete(this.createFileStream);
+        }
+
+        [TestInitialize]
+        public void Initialise()
+        {
+            this.createFileStream = Path.Combine(Path.GetTempPath(), "createstream_" + Guid.NewGuid().ToString());
+        }
+        
         [TestMethod]
         public void Can_write_with_stream()
         {
